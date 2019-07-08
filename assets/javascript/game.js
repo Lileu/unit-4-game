d65e0d8b8e665c364189642ee63bc0dd1ebf7229
Lisa Leu <Lisal@netwealth.com.au>
2019-07-08 17:34:38 +1000

    added basic html structure and elements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,22 @@
 //* The random number shown at the start of the game should be between 19 - 120.
 
 //* Each crystal should have a random hidden value between 1 - 12.
+
+
+// GLOBAL VARIABLES
+
+// Scores
+var currentScore = 0;
+var winScore = 0;
+
+// Wins and Losses
+var winCont = 0;
+var lossCount = o;
+
+
+
+// FUNCTIONS
+
+
+
+// MAIN PROCESS
