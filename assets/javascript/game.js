6791c21bc953e817b3f654c94476311bb783b9be
Lisa Leu <Lisal@netwealth.com.au>
2019-07-09 16:21:15 +1000

    created on-click event attached to the crystal buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 //$(document).ready(function() {
 // GLOBAL VARIABLES
 
-var currentScore = 0; //#
-var winScore = 0; //#
+var currentScore = 0; //#progress-number
+var winScore = 0; //#goal-number
 var winCount = 0; //#win-count
 var lossCount = 0; //#loss-count
 
@@ -28,9 +28,29 @@ for (i = 0; i < crystalValuesArr.length; i++) {
     // 4. Then give each "crystalBtn" a data-attribute called "data-crystalValue".
     crystalBtn.attr("data-crystalValue", crystalValuesArr.length[i]);
 
-    // 6. Finally, append each "letterBtn" to the "#buttons" div (provided).
-    $("#buttons").append(crystalBtn);
+    // 6. Finally, append each "crystalBtn" to the ".far" div 
+    $(".far").append(crystalBtn);
 
-    //console.log($(this));
-   // console.log(this);
 }
+
+
+ // MAJOR TASK #2: ATTACH ON-CLICK EVENTS TO "LETTER" BUTTONS
+      // =================================================================================
+
+      // 7. Create an "on-click" event attached to the ".letter-button" class.
+      $(".crystal-icon").on("click", function() {
+
+        //retrieve the random number assigned to the variable and declare as crystalValue variable
+        var crystalValue = ($("this").attr("data-crystalValue"));
+        // and parse as an integer
+        crystalValue = parseInt(crystalValue);
+        //add the crystal value to the current score
+        currentScore += crystalValue;
+        //updating the 'Your Progress' number
+        $("#progress-number").text(currentScore);
+        //logging
+        console.log(crystalValue);
+        console.log(currentScore);
+    })
+
+   
