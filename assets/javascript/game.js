63e8f8456586fbefa43eadc3881c2fdd71fec95b
Lisa Leu <Lisal@netwealth.com.au>
2019-07-09 11:36:38 +1000

    added crystal images to html and variables to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@
 
 //* Each crystal should have a random hidden value between 1 - 12.
 
-
+$(document).ready(function() {
 // GLOBAL VARIABLES
 
 // Scores
@@ -13,12 +13,16 @@ var winScore = 0;
 
 // Wins and Losses
 var winCont = 0;
-var lossCount = o;
+var lossCount = 0;
+
+var imagesArr = ["assets/images/blue-crystal.svg","assets/images/yellow-crystal.svg","assets/images/purple-crystal.svg","assets/images/green-crystal.svg"]  
+]
 
 
 
 // FUNCTIONS
-
+function randomCrystal
+Math.floor(Math.random() * (12 - 1)) + 1
 
 
 // MAIN PROCESS
